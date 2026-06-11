b7653b80d42fb9dd537599d9f59592691324a4e0
Hector Rivera <hector@hyrivera.com>
2026-06-11 18:16:24 -0400

    Handle missing spotlight content in scraper

--- pages/detailed/spotlight.js
+++ pages/detailed/spotlight.js
@@ -6,6 +6,7 @@ async function get(url, id, bkp) {
         const dom = await JSDOM.fromURL(url, {});
 
         var content = dom.window.document.querySelectorAll('.pkmn-list-flex');
+        var description = dom.window.document.querySelector('.event-description');
 
         var spotlight = {
             name: "",
@@ -15,29 +16,50 @@ async function get(url, id, bkp) {
             list: []
         };
 
-        spotlight.name = content[0].querySelector(":scope > .pkmn-list-item > .pkmn-name").innerHTML;
-        spotlight.canBeShiny = content[0].querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
-        spotlight.image = content[0].querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img").src;
+        if (content.length > 0) {
+            spotlight.name = content[0].querySelector(":scope > .pkmn-list-item > .pkmn-name")?.innerHTML || "";
+            spotlight.canBeShiny = content[0].querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
+            spotlight.image = content[0].querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img")?.src || "";
 
-        var temp = dom.window.document.querySelectorAll('.event-description')[0].innerHTML;
-        var split = temp.split("<strong>");
-        spotlight.bonus = split[split.length - 1].split("</strong>")[0];
+            dom.window.document.querySelectorAll(".pkmn-list-item").forEach(p => {
+                var pokemon = {
+                    name: p.querySelector(":scope > .pkmn-name")?.innerHTML || "",
+                    canBeShiny: p.querySelector(":scope > .shiny-icon") != null,
+                    image: p.querySelector(":scope > .pkmn-list-img > img")?.src || ""
+                }
+                spotlight.list.push(pokemon);
+            });
+        } else if (description) {
+            var strongText = Array.from(description.querySelectorAll("strong")).map(s => s.textContent.trim());
+            var title = dom.window.document.querySelector(".page-title")?.textContent.trim().replace(/\s+/g, " ") || "";
 
-        dom.window.document.querySelectorAll(".pkmn-list-item").forEach(p => {
-            var pokemon = {
-                name: p.querySelector(":scope > .pkmn-name").innerHTML,
-                canBeShiny: p.querySelector(":scope > .shiny-icon") != null,
-                image: p.querySelector(":scope > .pkmn-list-img > img").src
+            spotlight.name = strongText.length > 1 ? strongText[1] : title.replace(/\s*Spotlight\s+Hour\s*$/i, "");
+            spotlight.bonus = strongText.length > 2 ? strongText[2] : "";
+
+            if (spotlight.name) {
+                spotlight.list.push({
+                    name: spotlight.name,
+                    canBeShiny: spotlight.canBeShiny,
+                    image: spotlight.image
+                });
             }
-            spotlight.list.push(pokemon);
-        });
+        }
 
-        await fsp.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: spotlight }));
+        if (!spotlight.bonus && description) {
+            var temp = description.innerHTML;
+            var split = temp.split("<strong>");
+            spotlight.bonus = split[split.length - 1].split("</strong>")[0];
+        }
+
+        if (spotlight.name || spotlight.bonus || spotlight.list.length > 0) {
+            await fsp.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: spotlight }));
+        }
     } catch (err) {
         console.error(err);
         for (var i = 0; i < bkp.length; i++) {
-            if (bkp[i].eventID == id) {
-                await fsp.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: bkp[i].extraData.spotlight.data }));
+            if (bkp[i].eventID == id && bkp[i].extraData != null && 'spotlight' in bkp[i].extraData) {
+                var fallbackData = bkp[i].extraData.spotlight.data || bkp[i].extraData.spotlight;
+                await fsp.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: fallbackData }));
             }
         }
     }
